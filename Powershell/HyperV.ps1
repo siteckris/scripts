1fe63d0e3022655176852e415fe25426fc241210
siteckris <post@sitec.dk>
2022-11-06 15:35:20 +0100

    Update HyperV.ps1

--- Powershell/HyperV.ps1
+++ Powershell/HyperV.ps1
@@ -1,9 +1,9 @@
 #List Snapshots
-Get-VM -ComputerName (Get-ClusterNode -Cluster Ka-Hypv01.dkpsnet.adv) | Get-VMSnapshot | Select VMName,Name,SnapshotType,CreationTime,ComputerName
+Get-VM -ComputerName (Get-ClusterNode -Cluster "FQDNHypVClustername") | Get-VMSnapshot | Select VMName,Name,SnapshotType,CreationTime,ComputerName
 
 # Remove Snapshot 1
-Get-VMSnapshot -ComputerName "MyHyperVHost" -VMName "KA-WSUS01" | Remove-VMSnapshot
+Get-VMSnapshot -ComputerName "MyHyperVHost" -VMName "ServerName" | Remove-VMSnapshot
 
 
 # Remove Snapshot 2
-Get-VMSnapshot -VMName "KA-WSUS01" | Remove-VMSnapshot
+Get-VMSnapshot -VMName "ServerName" | Remove-VMSnapshot
