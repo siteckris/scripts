79ee27da3f54b36f281ffccff59f0c9657f3c636
siteckris <post@sitec.dk>
2022-12-18 15:53:37 +0100

    Update AbsoluteSnips-AD.ps1
    Added FSMO roles related lines

--- Powershell/AD/AbsoluteSnips-AD.ps1
+++ Powershell/AD/AbsoluteSnips-AD.ps1
@@ -1,2 +1,16 @@
 #Get lst of groups a user is member off
 Get-ADPrincipalGroupMembership -Username
+
+#List domain wide FSMO Roles
+Get-ADForest | Select-object DomainNamingMaster,SchemaMaster | FormatList
+
+#List Fores Wide FSMO Roles
+Get-ADDomain | Slect-Object InfrastructureMaster,PDCEmulator,RIDMaster | Format-List
+
+#Move an FSMO Role
+Move-ADDirectoryServerOperationRole - Identity "dc2" -OperationMasterRole PDCEmulator
+
+#Seize en FSMO Role
+Move-ADDirectoryServerOperationRole - Identity "dc2" -OperationMasterRole PDCEmulator -Force
+#if you seize a role the server you seize it from, should never go online again.
+
