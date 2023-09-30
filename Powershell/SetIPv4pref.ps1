1fb7fb8618e99d559846727a6bfcdddb919464da
siteckris <post@sitec.dk>
2023-09-30 11:14:19 +0200

    Update SetIPv4pref.ps1
    opdateret beskrivelse

--- Powershell/SetIPv4pref.ps1
+++ Powershell/SetIPv4pref.ps1
@@ -6,7 +6,7 @@ ping $env:COMPUTERNAME
 #If the reply is IPv6 address, run following registry setting to just prefer ipv4 and reboot
 New-ItemProperty “HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\” -Name “DisabledComponents” -Value 0x20 -PropertyType “DWord”
 
-#If DisabledComponents exists, use the set cmdlet
+#If DisabledComponents exists, use the set cmdlet instead
 Set-ItemProperty “HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\” -Name “DisabledComponents” -Value 0x20
 
 #You need to reboot the computer in order for the changes to take effect
