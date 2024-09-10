19c4f93ca26e898ef234ab7eb1c32e7ccf8a6477
siteckris <post@sitec.dk>
2024-09-10 09:35:21 +0200

    Update AbsoluteSnips-AD.ps1
    Opdateret Templates og mere

--- Powershell/AD/AbsoluteSnips-AD.ps1
+++ Powershell/AD/AbsoluteSnips-AD.ps1
@@ -1,16 +1,22 @@
-#Get lst of groups a user is member off
+# Get lst of groups a user is member off
 Get-ADPrincipalGroupMembership -Username
 
-#List domain wide FSMO Roles
+# List domain wide FSMO Roles
 Get-ADForest | Select-object DomainNamingMaster,SchemaMaster | FormatList
 
-#List Fores Wide FSMO Roles
+# List Fores Wide FSMO Roles
 Get-ADDomain | Slect-Object InfrastructureMaster,PDCEmulator,RIDMaster | Format-List
 
-#Move an FSMO Role
+# Move an FSMO Role
 Move-ADDirectoryServerOperationRole - Identity "dc2" -OperationMasterRole PDCEmulator
 
-#Seize en FSMO Role
+# Seize en FSMO Role
 Move-ADDirectoryServerOperationRole - Identity "dc2" -OperationMasterRole PDCEmulator -Force
 #if you seize a role the server you seize it from, should never go online again.
+
+# Location of GPO templatete files
+# \\contoso.com\SYSVOL\%domain%\policies\PolicyDefinitions
+
+# Location of Scripts executed my domain
+# C:\Windows\SYSVOL\%domain%\scripts
 
